f01381c08fe63c4a89194a50ba49f7f6e2085ec0
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-03 08:11:33 +0500

    first commit

---
+++ question1.py
@@ -0,0 +1,15 @@
+def calculate_ages():
+    anton_age = 21
+    beth_age = anton_age
+    chen_age = beth_age + 20
+    drew_age = chen_age + anton_age
+    
+    
+    ethan_age = chen_age
+    print(f"Anton is {anton_age} years old.")
+    print(f"Beth is {beth_age} years old.")
+    print(f"Chen is {chen_age} years old.")
+    print(f"Drew is {drew_age} years old.")
+    print(f"Ethan is {ethan_age} years old.")
+calculate_ages()
+
